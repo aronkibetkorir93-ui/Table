07e79ed63d93eb2b0185aec262df1bf0629ec46e
aronkibetkorir93-ui <aronkibetkorir93@gmail.com>
2026-04-25 15:03:53 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,64 +1,36 @@
-const players = ["Kibet Aron", "Bralyn Kipkirui", "Manu Kheed", "Lino", "Holyplug", "Manu Josh", "Blamek", "Ian Too"];
-const ADMIN_PASSWORD = "EliteAdmin2026";
+const players = ["KIBET ARON", "BRALYN KIPKIRUI", "MANU KHEED", "LINO", "HOLYPLUG", "MANU JOSH", "BLAMEK", "IAN TOO"];
 
+// Initial empty fixtures
 let fixtures = [
-    { r: 1, p1: "Kibet Aron", p2: "Ian Too", s1: null, s2: null },
-    { r: 1, p1: "Bralyn Kipkirui", p2: "Blamek", s1: null, s2: null },
-    { r: 1, p1: "Manu Kheed", p2: "Manu Josh", s1: null, s2: null },
-    { r: 1, p1: "Lino", p2: "Holyplug", s1: null, s2: null },
-    { r: 2, p1: "Ian Too", p2: "Holyplug", s1: null, s2: null },
-    { r: 2, p1: "Manu Josh", p2: "Lino", s1: null, s2: null },
-    { r: 2, p1: "Blamek", p2: "Manu Kheed", s1: null, s2: null },
-    { r: 2, p1: "Kibet Aron", p2: "Bralyn Kipkirui", s1: null, s2: null },
-    { r: 3, p1: "Bralyn Kipkirui", p2: "Ian Too", s1: null, s2: null },
-    { r: 3, p1: "Manu Kheed", p2: "Kibet Aron", s1: null, s2: null },
-    { r: 3, p1: "Lino", p2: "Blamek", s1: null, s2: null },
-    { r: 3, p1: "Holyplug", p2: "Manu Josh", s1: null, s2: null },
-    { r: 4, p1: "Ian Too", p2: "Manu Josh", s1: null, s2: null },
-    { r: 4, p1: "Blamek", p2: "Holyplug", s1: null, s2: null },
-    { r: 4, p1: "Kibet Aron", p2: "Lino", s1: null, s2: null },
-    { r: 4, p1: "Bralyn Kipkirui", p2: "Manu Kheed", s1: null, s2: null },
-    { r: 5, p1: "Manu Kheed", p2: "Ian Too", s1: null, s2: null },
-    { r: 5, p1: "Lino", p2: "Bralyn Kipkirui", s1: null, s2: null },
-    { r: 5, p1: "Holyplug", p2: "Kibet Aron", s1: null, s2: null },
-    { r: 5, p1: "Manu Josh", p2: "Blamek", s1: null, s2: null },
-    { r: 6, p1: "Ian Too", p2: "Blamek", s1: null, s2: null },
-    { r: 6, p1: "Kibet Aron", p2: "Manu Josh", s1: null, s2: null },
-    { r: 6, p1: "Bralyn Kipkirui", p2: "Holyplug", s1: null, s2: null },
-    { r: 6, p1: "Manu Kheed", p2: "Lino", s1: null, s2: null },
-    { r: 7, p1: "Lino", p2: "Ian Too", s1: null, s2: null },
-    { r: 7, p1: "Holyplug", p2: "Manu Kheed", s1: null, s2: null },
-    { r: 7, p1: "Manu Josh", p2: "Bralyn Kipkirui", s1: null, s2: null },
-    { r: 7, p1: "Blamek", p2: "Kibet Aron", s1: null, s2: null }
+    { p1: "KIBET ARON", p2: "IAN TOO", s1: null, s2: null },
+    { p1: "BRALYN KIPKIRUI", p2: "BLAMEK", s1: null, s2: null },
+    { p1: "MANU KHEED", p2: "MANU JOSH", s1: null, s2: null },
+    { p1: "LINO", p2: "HOLYPLUG", s1: null, s2: null },
+    { p1: "IAN TOO", p2: "HOLYPLUG", s1: null, s2: null },
+    { p1: "MANU JOSH", p2: "LINO", s1: null, s2: null },
+    { p1: "BLAMEK", p2: "MANU KHEED", s1: null, s2: null },
+    { p1: "KIBET ARON", p2: "BRALYN KIPKIRUI", s1: null, s2: null }
 ];
 
-let isAdmin = false;
-
 function unlockAdmin() {
-    if (prompt("Enter Password:") === ADMIN_PASSWORD) {
-        isAdmin = true;
-        document.getElementById('saveBtn').classList.remove('hidden');
+    if (prompt("PASSWORD:") === "EliteAdmin2026") {
+        document.getElementById('adminSection').classList.remove('hidden');
         renderFixtures();
     }
 }
 
 function renderFixtures() {
-    const container = document.getElementById('fixtureContainer');
-    container.innerHTML = '';
-    let currentR = 0;
+    const list = document.getElementById('fixtureList');
+    list.innerHTML = '';
     fixtures.forEach((f, i) => {
-        if (f.r !== currentR) {
-            currentR = f.r;
-            container.innerHTML += `<div style="color:var(--gold); padding:10px 0; border-bottom:1px solid #1e293b">WEEK ${currentR}</div>`;
-        }
-        container.innerHTML += `
-            <div class="match">
-                <span style="width:35%; text-align:right">${f.p1}</span>
-                <div style="display:flex; gap:4px">
-                    <input type="number" value="${f.s1!==null?f.s1:''}" ${!isAdmin?'readonly':''} onchange="updateScore(${i},'s1',this.value)">
-                    <input type="number" value="${f.s2!==null?f.s2:''}" ${!isAdmin?'readonly':''} onchange="updateScore(${i},'s2',this.value)">
+        list.innerHTML += `
+            <div class="match-row">
+                <span style="width:38%; text-align:right;">${f.p1}</span>
+                <div style="display:flex; gap:5px;">
+                    <input type="number" value="${f.s1!==null?f.s1:''}" onchange="updateScore(${i},'s1',this.value)">
+                    <input type="number" value="${f.s2!==null?f.s2:''}" onchange="updateScore(${i},'s2',this.value)">
                 </div>
-                <span style="width:35%">${f.p2}</span>
+                <span style="width:38%;">${f.p2}</span>
             </div>`;
     });
 }
@@ -87,47 +59,43 @@ function calculateTable() {
 
     const sorted = Object.entries(stats).sort((a,b) => b[1].pts - a[1].pts || b[1].gd - a[1].gd);
     
+    // Update Awards
     const ts = Object.entries(stats).sort((a,b) => b[1].gf - a[1].gf)[0];
     const bd = Object.entries(stats).filter(x => x[1].p > 0).sort((a,b) => a[1].ga - b[1].ga)[0];
     document.getElementById('topScorer').innerText = ts[1].gf > 0 ? `${ts[0]} (${ts[1].gf})` : "---";
     document.getElementById('bestDefense').innerText = bd ? `${bd[0]} (${bd[1].ga} GA)` : "---";
 
-    const tbody = document.getElementById('tableBody');
-    tbody.innerHTML = '';
+    const body = document.getElementById('tableBody');
+    body.innerHTML = '';
     sorted.forEach((item, i) => {
         const [name, s] = item;
-        tbody.innerHTML += `
-            <div class="grid-row">
-                <div class="c-pos">${i+1}</div>
-                <div class="c-name">${name}</div>
-                <div class="c-s">${s.p}</div>
-                <div class="c-s">${s.w}</div>
-                <div class="c-s">${s.d}</div>
-                <div class="c-s">${s.l}</div>
-                <div class="c-s">${s.gf}</div>
-                <div class="c-s">${s.ga}</div>
-                <div class="c-s">${s.gd}</div>
-                <div class="c-pts">${s.pts}</div>
+        body.innerHTML += `
+            <div class="t-row">
+                <div class="col-pos">${i+1}</div>
+                <div class="col-name">${name}</div>
+                <div class="col-s">${s.p}</div><div class="col-s">${s.w}</div><div class="col-s">${s.d}</div><div class="col-s">${s.l}</div>
+                <div class="col-s">${s.gf}</div><div class="col-s">${s.ga}</div><div class="col-s">${s.gd}</div>
+                <div class="col-pts">${s.pts}</div>
             </div>`;
     });
 }
 
 function downloadTable() {
-    html2canvas(document.querySelector("#captureArea")).then(canvas => {
+    html2canvas(document.getElementById('captureArea'), { backgroundColor: "#020617", scale: 2 }).then(canvas => {
         let link = document.createElement('a');
-        link.download = 'EFL_Standings.png';
+        link.download = 'EFL_Standings_Season1.png';
         link.href = canvas.toDataURL();
         link.click();
     });
 }
 
 function saveData() {
-    localStorage.setItem('efl_v_fixed', JSON.stringify(fixtures));
-    alert("Updated Standings!");
+    localStorage.setItem('efl_final_v6', JSON.stringify(fixtures));
+    alert("SCORES PUBLISHED SUCCESSFULLY!");
 }
 
-const local = localStorage.getItem('efl_v_fixed');
+// Load data on start
+const local = localStorage.getItem('efl_final_v6');
 if (local) fixtures = JSON.parse(local);
-renderFixtures();
 calculateTable();
         
